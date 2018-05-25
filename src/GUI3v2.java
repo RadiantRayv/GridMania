fd39bce2ba5ea9db56c5b7b2385e50216a7a755c
RadiantRayv <RadiantRayv@users.noreply.github.com>
2018-05-24 21:55:24 -0700

    5/24 at school

---
+++ src/GUI3v2.java
@@ -0,0 +1,153 @@
+import java.awt.*;          // access to Container
+import java.awt.event.*;    // access to WindowAdapter, WindowEvent
+import javax.swing.*;
+import java.net.URL;
+
+public class GUI3v2 extends JFrame
+{ 
+
+	private Image square;
+	private Image red;
+	private Image grid;
+	private int counter;         // counts seconds
+	private int sqX, sqY, sqX2, sqY2, sqX3, sqY3;
+	private int redX, redY;
+	private int redSize;
+	private int size, size2, size3;
+	
+	private long TimeStart;
+	private long timediff;
+
+	public GUI3v2() 
+	{
+		super("Demo Graphics: Grid, Square, Red Square");
+		ClassLoader cldr = this.getClass().getClassLoader();
+		square = new ImageIcon(cldr.getResource("bluesquare.png")).getImage();
+		red = new ImageIcon(cldr.getResource("redsquare.png")).getImage();
+		grid = new ImageIcon(cldr.getResource("grid.png")).getImage();
+		addWindowListener(new java.awt.event.WindowAdapter() {public void windowClosing(WindowEvent evt) {System.exit(0);}});
+		sqX = 82;
+		sqY = 82;
+		sqX2 = 233;
+		sqY2 = 157;
+		sqX3 = 158;
+		sqY3 = 232;
+		redX = 157;
+		redY = 158;
+		setSize(500, 500);
+		setVisible(true);
+		size = 10;
+		size2 = 10;
+		size3 = 10;
+		redSize = 10;
+		
+		counter = 0;
+	}
+
+	/*
+	public void paint (Graphics g )
+	{
+		super.paint(g);
+		g.drawImage(square, sqX, sqY, size, size, this);
+		g.drawImage(grid, 50, 50, this);
+
+	}
+	*/
+	
+	public void paint(Graphics g)
+	{
+		Image offImage = createImage(1000, 1000);
+	// Creates an off-screen drawable image to be used for
+	// double buffering; XSIZE, YSIZE are each of type ‘int’;
+	// represents size of JFrame or JPanel, etc
+		Graphics buffer = offImage.getGraphics();
+	// Creates a graphics context for drawing to an 
+	// off-screen image
+		paintOffScreen(buffer);		// your own method
+		g.drawImage(offImage, 0, 0, null);	
+	// draws the image with upper left corner at 0,0
+	}
+
+	public void paintOffScreen(Graphics g)
+	{
+		// sometimes helpful to do this first to clear things:
+		g.clearRect(0, 0, 500, 500);
+		g.drawImage(grid, 50, 50, this);
+		if (counter < 42)
+		{
+			g.drawImage(square, sqX, sqY, size, size, this);
+		}
+		if (counter > 20 && counter < 60)
+		{
+			g.drawImage(square, sqX2, sqY2, size2, size2, this);
+		}
+		if (counter > 40 && counter < 80)
+		{
+			g.drawImage(square, sqX3, sqY3, size3, size3, this);
+		}
+		if (counter < 80)
+		{
+			g.drawImage(red, redX, redY, redSize, redSize, this);
+		}
+		
+	}
+	
+	public void update(long TimeStart)
+	{
+			timediff = (System.currentTimeMillis() - TimeStart);
+			if(timediff % 25 == 0)
+			{
+				heck();
+			}
+			while(System.currentTimeMillis() - TimeStart == timediff)
+			{
+			}
+	}
+
+
+	public void heck()
+	{
+		if(size < 75)
+		{
+			sqX -= (timediff/25);
+			sqY -= (timediff/25);
+			size += 2*(timediff/25);
+		}
+		if (size >= 50 && size2 < 75)
+		{
+			sqX2 -= (timediff/25);
+			sqY2 -= (timediff/25);
+			size2 += 2*(timediff/25);
+		}
+		if (size2 >= 50 && size3 < 75)
+		{
+			sqX3 -= (timediff/25);
+			sqY3 -= (timediff/25);
+			size3 += 2*(timediff/25);
+		}
+		if (size3 < 15)
+		{
+			redX -= (timediff/25)*3;
+			redY -= (timediff/25)*3;
+			redSize += 6*(timediff/25);
+		}
+//		counter++;
+//		System.out.println("time is " + counter);
+		repaint();
+	}
+
+	public static void main(String[] args) 
+	{
+		GUI3v2 application = new GUI3v2();
+//		double x=0;
+////		while(x<1000000000)
+////		{
+////			x += 0.5;
+////		}
+		long tTimeStart = System.currentTimeMillis();
+		while(true)
+		{
+		application.update(tTimeStart);
+		}
+	}
+}
