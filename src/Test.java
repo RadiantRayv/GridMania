5f6ed502178a524695d7364d2c64dd666d22a3d2
ObiWanKenobi729 <38986252+ObiWanKenobi729@users.noreply.github.com>
2018-05-29 09:31:19 -0700

    tried to size but to no avail

--- src/Test.java
+++ src/Test.java
@@ -192,7 +192,8 @@ public class Test implements ActionListener  {
 	 */
 	private static void createAndShowGUI() {
 		//Create and set up the window.
-		JFrame frame = new JFrame("CardLayoutDemo");
+		JFrame frame = new JFrame("Grid Beats");
+		frame.setSize(555, 555);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		//Create and set up the content pane.
